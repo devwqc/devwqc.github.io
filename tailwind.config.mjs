79ad285d2f2e862fdeebfbf2d37fef6100adcae5
devwqc <98067115+devwqc@users.noreply.github.com>
2024-10-01 21:17:56 +0900

    chore: typography default max-width: 100%

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
